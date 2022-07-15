caf80ad145d1300a0696d9e364e56a0d4a238bcc
Crowdbotics <team@crowdbotics.com>
2022-07-15 18:42:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "enduredfit_36206",
+  url: "https://enduredfit_36206.botics.co",
+  api: "https://enduredfit_36206.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
